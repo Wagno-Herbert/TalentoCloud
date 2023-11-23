8b43ce2593a046fafd9ff4b3fa31e3f903df7e05
Wagno Herbert de Jesus Santana <148294975+Wagno-Herbert@users.noreply.github.com>
2023-11-22 22:05:17 -0300

    Create desenvol_6ano_nascimento.py

---
+++ desenvol_6ano_nascimento.py
@@ -0,0 +1,28 @@
+# TalentoCloud 
+def calcular_idade(ano_nascimento):
+    ano_atual = 2022
+    idade = ano_atual - ano_nascimento
+    return idade
+
+def obter_ano_nascimento():
+    while True:
+        try:
+            ano_nascimento = int(input("Digite o ano de nascimento (1922 a 2021): "))
+            if 1922 <= ano_nascimento <= 2021:
+                return ano_nascimento
+            else:
+                print("Ano de nascimento fora do intervalo permitido. Tente novamente.")
+        except ValueError:
+            print("Erro: Insira um valor numérico válido.")
+
+def main():
+    nome_completo = input("Digite seu nome completo: ")
+    ano_nascimento = obter_ano_nascimento()
+
+    idade = calcular_idade(ano_nascimento)
+
+    print(f"\nNome: {nome_completo}")
+    print(f"Idade em 2022: {idade} anos")
+
+if __name__ == "__main__":
+    main()
